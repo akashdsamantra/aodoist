1332490ca027597a29b64ce127b5afae0d5e7ca4
Akashdeep Samantra <akash.samantra19@gmail.com>
2020-11-28 22:31:00 +0530

    Create auth layout

---
+++ packages/web-client/src/app/layouts/Auth.layout.svelte
@@ -0,0 +1,19 @@
+<script>
+</script>
+
+<style>
+    .container {
+        text-align: center;
+        width: 500px;
+        position: absolute;
+        top: 40%;
+        left: 50%;
+        transform: translate(-50%, -40%);
+    }
+</style>
+
+<div>
+    <div class="container">
+        <slot />
+    </div>
+</div>
